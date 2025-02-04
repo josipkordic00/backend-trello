f3dc2b754c7a2e3f073f63716c1b614bedf6a772
josipkordic00 <josipkordic00@gmail.com>
2025-02-04 13:11:57 +0100

    fin

--- src/main/java/com/faketrello/trelloback/services/CardService.java
+++ src/main/java/com/faketrello/trelloback/services/CardService.java
@@ -13,10 +13,10 @@ public class CardService {
 
     @Autowired
     public CardDAO cardDAO;
-    public String nesto;
+
 
     public List<Card> getAllCardsByTaskListId(Long taskListId){
-        System.out.println("upgrades");
+
         return cardDAO.getAllCardsByTaskListId(taskListId);
     }
 }
